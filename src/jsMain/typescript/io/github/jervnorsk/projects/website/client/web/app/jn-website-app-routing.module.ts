444937e24d30e7bf8df5c7453c2190e371b78949
JervNorsk <jerv.norsk@gmail.com>
2023-08-10 19:09:50 +0200

    Enable useHash in Angular routing

--- src/jsMain/typescript/io/github/jervnorsk/projects/website/client/web/app/jn-website-app-routing.module.ts
+++ src/jsMain/typescript/io/github/jervnorsk/projects/website/client/web/app/jn-website-app-routing.module.ts
@@ -50,7 +50,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {enableTracing: false})],
+    imports: [RouterModule.forRoot(routes, {useHash: true, enableTracing: false})],
     exports: [RouterModule]
 })
 export class JnWebsiteAppRoutingModule {
